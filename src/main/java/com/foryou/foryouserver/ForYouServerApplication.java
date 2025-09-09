bd0de8eb81ec23b769b1870020687fd9ddf5bb48
이준형 <leejh@mslk.co.kr>
2025-09-09 17:00:48 +0900

    초기 데이터 추가

---
+++ src/main/java/com/foryou/foryouserver/ForYouServerApplication.java
@@ -0,0 +1,13 @@
+package com.foryou.foryouserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForYouServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForYouServerApplication.class, args);
+    }
+
+}
