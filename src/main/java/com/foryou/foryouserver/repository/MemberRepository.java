714d8cbc5bdc40913e6741cc0679b55af38ebcf4
sangbin <tkdqls612@naver.com>
2025-10-12 15:24:02 +0900

    refactor: 리포지토리 주석

--- src/main/java/com/foryou/foryouserver/repository/MemberRepository.java
+++ src/main/java/com/foryou/foryouserver/repository/MemberRepository.java
@@ -9,11 +9,25 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<MemMst, Long>{
+    /*
+    * 기능 : 이메일 중복 체크
+    * return : boolean
+    * 사용 위치 : MemberService
+    * */
     boolean existsByMemEmail(String memEmail);
 
-    Optional<MemMst> findByMemEmail(String memEmail);
+    /*
+    * 기능 : 회원 상태로 리스트 조회
+    * return : List<MemMst>
+    * 사용 위치 : MemberService
+    * */
     List<MemMst> findByMemStatus(String memStatus);
 
-    // OAuth2 로그인용 메서드
+    //
+    /*
+    * 기능 : OAuth2 로그인용 메서드
+    * return : Optional<MemMst>
+    * 사용 위치 : CustomOAUTH2UserService.java
+    *  */
     Optional<MemMst> findByProviderAndProviderId(String provider, String providerId);
 }
